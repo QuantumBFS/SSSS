1e6444da20f4ecafcf032572741a7b83ce866ad3
Roger-luo <hiroger@qq.com>
2019-04-25 13:51:03 +0800

    make SSSS loadable inside REPL

--- src/SSSS.jl
+++ src/SSSS.jl
@@ -1,5 +1,35 @@
 module SSSS
 
-greet() = print("Hello World!")
+using IJulia, Pkg
+
+export notebooks
+
+# function tutorial()
+#     cmd = IJulia.find_jupyter_subcommand("notebook")
+#     push!(cmd.exec, joinpath(@__DIR__, "..", "notebooks", "tutorial.ipynb"))
+#     return IJulia.launch(cmd, joinpath(@__DIR__, "..", "notebooks"), false)
+# end
+
+function notebooks()
+    return IJulia.notebook(dir=joinpath(@__DIR__, "..", "notebooks"))
+end
+
+REQUIRE = [
+    "GR",
+    "PyCall",
+    "IJulia",
+    "Revise",
+    "Interact",
+]
+
+function __init__()
+    for each in REQUIRE
+        if each in keys(Pkg.installed())
+            continue
+        else
+            Pkg.add(each)
+        end
+    end
+end
 
 end # module
