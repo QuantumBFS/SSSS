70bf8f98a066b28d10ec78dc12824443e8d4ffe4
Lei Wang <wangleiphy@gmail.com>
2019-04-30 08:47:55 +0800

    fix y0=0

--- 1_dl/brachistochrone.py
+++ 1_dl/brachistochrone.py
@@ -7,14 +7,13 @@ import numpy as np
 from torchdiffeq import odeint_adjoint as odeint
 
 class MLP(nn.Module):
-    def __init__(self, hidden_size, y0=0.0, y1=1.0):
+    def __init__(self, hidden_size, y1=1.0):
         super(MLP, self).__init__()
         self.fc1 = nn.Linear(1, hidden_size)
         self.fc2 = nn.Linear(hidden_size, hidden_size)
         self.fc3 = nn.Linear(hidden_size, 1)
 
-        self.y0 = y0
-        self.y1 = y1 
+        self.y1 = y1
 
     def _f(self, x):
         out = F.softplus(self.fc1(x))
@@ -23,9 +22,13 @@ class MLP(nn.Module):
         return out.sum()
 
     def forward(self, x):
+        '''
+        y(0) = 0
+        y(1) = y1 
+        '''
         f0 = self._f(torch.tensor([0.0]))
         f1 = self._f(torch.tensor([1.0]))
-        return self._f(x) - (f0 + self.y1-self.y0)*(1.0-x) - f1*x + self.y1
+        return self._f(x) - (f0 + self.y1)*(1.0-x) - f1*x + self.y1
 
     def value_and_grad(self, x):
         y = self.forward(x)
@@ -45,10 +48,10 @@ class Brachistochrone(nn.Module):
 
 def plot(model):
     plt.cla()
-    xlist = torch.linspace(0.0, 1.0, 11)
+    xlist = torch.linspace(0.0, 1.0, 21)
     ylist = [model.net(torch.tensor([x])) for x in xlist]
-    plt.plot(xlist.numpy(), ylist)
-    plt.plot([0.0, 1.0], [model.net.y0, model.net.y1], 'r*', ms=20)
+    plt.plot(xlist.numpy(), ylist, lw=2)
+    plt.plot([0.0, 1.0], [0.0, model.net.y1], 'r*', ms=20)
     plt.gca().invert_yaxis()
 
     plt.xlabel('$x$')
@@ -59,12 +62,13 @@ def plot(model):
 
 if __name__ == '__main__':
     
-    g = 20.0  #gravity 
+    g = 10.0  #gravity 
     v0 = 1.0  #initial velocity
-    nh = 16 # number of hidden neurons
+    nh = 32 # number of hidden neurons
+    y1 = 1.0 # fininal y coordinate
 
-    model = Brachistochrone(g, v0, MLP(nh))
-    optimizer = optim.Rprop(model.parameters())
+    model = Brachistochrone(g, v0, MLP(nh, y1))
+    optimizer = optim.Adam(model.parameters(), lr=1E-2)
 
     import matplotlib.pyplot as plt
     # Set up figure.
