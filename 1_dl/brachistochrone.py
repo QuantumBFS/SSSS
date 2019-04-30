ef8cef954746af7ff022b7f8d46da90c69fd580f
Lei Wang <wangleiphy@gmail.com>
2019-04-30 08:13:43 +0800

    initial velocity

--- 1_dl/brachistochrone.py
+++ 1_dl/brachistochrone.py
@@ -7,11 +7,14 @@ import numpy as np
 from torchdiffeq import odeint_adjoint as odeint
 
 class MLP(nn.Module):
-    def __init__(self, hidden_size):
+    def __init__(self, hidden_size, y0=0.0, y1=1.0):
         super(MLP, self).__init__()
         self.fc1 = nn.Linear(1, hidden_size)
         self.fc2 = nn.Linear(hidden_size, hidden_size)
         self.fc3 = nn.Linear(hidden_size, 1)
+
+        self.y0 = y0
+        self.y1 = y1 
 
     def _f(self, x):
         out = F.softplus(self.fc1(x))
@@ -22,35 +25,45 @@ class MLP(nn.Module):
     def forward(self, x):
         f0 = self._f(torch.tensor([0.0]))
         f1 = self._f(torch.tensor([1.0]))
-        return self._f(x) - (f0-1.0)*(1.0-x) - f1*x + 1.0
+        return self._f(x) - (f0 + self.y1-self.y0)*(1.0-x) - f1*x + self.y1
 
-    def grad(self, x):
-        return torch.autograd.grad(self.forward(x), x, grad_outputs=torch.ones(x.shape[0]), create_graph=True)[0]
-
+    def value_and_grad(self, x):
+        y = self.forward(x)
+        return y, torch.autograd.grad(y, x, grad_outputs=torch.ones(x.shape[0]), create_graph=True)[0]
 
 class Brachistochrone(nn.Module):
-    def __init__(self, net):
+    def __init__(self, g, v0, net):
         super(Brachistochrone, self).__init__()
+        self.v0 = v0
+        self.g = g 
         self.net = net
 
     def forward(self, x, t):
         with torch.enable_grad():
-            y = self.net(x.view(-1))
-            dydx = self.net.grad(x.view(-1).detach().requires_grad_())
-        return torch.sqrt((1+dydx**2)/(2*0.1*y))
+            y, dydx = self.net.value_and_grad(x.view(-1).detach().requires_grad_())
+        return torch.sqrt((1+dydx**2)/(2*self.g*y+ self.v0**2)) 
 
 def plot(model):
     plt.cla()
     xlist = torch.linspace(0.0, 1.0, 11)
     ylist = [model.net(torch.tensor([x])) for x in xlist]
     plt.plot(xlist.numpy(), ylist)
+    plt.plot([0.0, 1.0], [model.net.y0, model.net.y1], 'r*', ms=20)
+    plt.gca().invert_yaxis()
+
+    plt.xlabel('$x$')
+    plt.ylabel('$y$')
 
     plt.draw()
     plt.pause(0.01)
 
 if __name__ == '__main__':
+    
+    g = 20.0  #gravity 
+    v0 = 1.0  #initial velocity
+    nh = 16 # number of hidden neurons
 
-    model = Brachistochrone(MLP(10))
+    model = Brachistochrone(g, v0, MLP(nh))
     optimizer = optim.Rprop(model.parameters())
 
     import matplotlib.pyplot as plt
@@ -63,7 +76,7 @@ if __name__ == '__main__':
     for epoch in range(100):
         optimizer.zero_grad()
         t = odeint(model, torch.tensor([0.0]), torch.tensor([0.0, 1.0]))
-        loss = t[1]
+        loss = t[1] - t[0]
         loss.backward()
         optimizer.step()
         print (epoch, loss.item())
